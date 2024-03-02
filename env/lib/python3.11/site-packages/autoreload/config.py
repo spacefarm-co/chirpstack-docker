402e244b7e04194ec1b420c1039f4cfdd4c9d13a
Soungno Kim <soungno@gmail.com>
2024-03-03 03:05:06 +0900

    sensecap setting

---
+++ env/lib/python3.11/site-packages/autoreload/config.py
@@ -0,0 +1,15 @@
+from enum import Enum, auto
+from dataclasses import dataclass
+
+
+class DriverFlavour(Enum):
+    Firefox = auto()
+    Chrome = auto()
+
+
+@dataclass(frozen=True)
+class Config:
+    driver_flavour: DriverFlavour
+    target_url: str
+    target_files: str
+    driver_path: str = None
